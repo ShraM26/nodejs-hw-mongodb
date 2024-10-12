28ed7f5717c2a654344e94d7973eed32a2cd72f5
shram <shram2615@gmail.com>
2024-10-12 17:20:15 +0300

    add Post put del

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,10 +1,22 @@
 import express from 'express';
-import { getAllContacts } from '../controllers/contactController.js';
-import { getContactById } from '../controllers/contactController.js';
+import { getAllContacts, getContactById, createContact, updateContact, deleteContact } from '../controllers/contactController.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
-router.get('/', getAllContacts);
-router.get('/:contactId', getContactById);
+// Маршрут для получения всех контактов
+router.get('/', ctrlWrapper( getAllContacts));
+
+// Маршрут для получения контакта по id
+router.get('/:contactId', ctrlWrapper( getContactById));
+
+// Маршрут для создания нового контакта
+router.post('/', ctrlWrapper( createContact));
+
+// Маршрут для обновления существующего контакта
+router.put('/:contactId', ctrlWrapper( updateContact));
+
+// Маршрут для удаления контакта
+router.delete('/:contactId', ctrlWrapper( deleteContact));
 
 export default router;
