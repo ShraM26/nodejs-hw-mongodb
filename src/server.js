59c33233d8d249a2103e9ff7c85a1edf1576aefc
shram <shram2615@gmail.com>
2024-11-12 13:00:25 +0200

    888

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ export function setupServer() {
   app.use('/contacts', contactRoutes);
 
   // Налаштування Swagger UI
-  const swaggerDocument = YAML.load(path.resolve('src', 'docs', 'openapi.yaml'));  // Вказуємо абсолютний шлях до openapi.yaml
+  const swaggerDocument = YAML.load(path.resolve('docs', 'openapi.yaml'));  // Вказуємо абсолютний шлях до openapi.yaml
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   // Логування запитів
